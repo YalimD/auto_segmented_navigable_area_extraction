17bc0b7060587dedd7059a5e67322f03507ae9a0
Yalım Doğan <yalimdre@gmail.com>
2019-09-02 22:55:20 +0300

    Dice coefficient added

--- SegmentationAssesment.py
+++ SegmentationAssesment.py
@@ -1,9 +1,8 @@
-# This method takes a segmented image and a ground truth image to determine the
-# ratio of false positives in all navigable areas. (False positive + true positive)
-import numpy as np
+# This method takes a segmented image and a ground truth image to determine the dice index
+
 import cv2
 
-# The size of the segmented image should be identical to ground truth
+# Note: The size of the segmented image should be identical to ground truth
 def assessSegmentation(segmented_img, ground_truth):
 
     # determine number of positives in segmented
@@ -17,12 +16,12 @@ def assessSegmentation(segmented_img, ground_truth):
     fp = num_of_navigable_segmented - tp
     fn = num_of_navigable_gt - tp
 
-    # There is your true and false positive values
-    print("Recall:{}, Precision:{}. For us, precision is more important".format(tp / (tp + fn), tp / (tp + fp)))
+    dice = (2 * tp) / (tp + fp + tp + fn)
+    print("Dice index: {}".format(dice))
 
 if __name__ == "__main__":
 
-    img = cv2.imread('C:\\Users\Rakosi\Google Drive\Master\ThesisWork\Thesis\Thesis_Final\Overleaf\clustering\80_2\stab_80_2_shadowClear_Mean_LAB.jpg', 0)
-    gt =  cv2.imread('C:\\Users\Rakosi\Google Drive\Master\ThesisWork\Thesis\Thesis_Final\Overleaf\clustering\80_2\stab_80_2_shadowClear_gt.png', 0)
+    img = cv2.imread('.\\test_images\\tf_test\\pet\\mask_pet.png', 0)
+    gt =  cv2.imread('.\\test_images\\tf_test\\pet\\pet_gt.png', 0)
 
     assessSegmentation(img, gt)
